81a950b32c807c2e0ce6c75b0c0fd58913e2441d
Fernando Espinosa <fer@ferdev.com>
2013-01-22 16:11:12 +0100

    Fixes broken spec

--- spec/acceptance/iati_basic_form_spec.rb
+++ spec/acceptance/iati_basic_form_spec.rb
@@ -126,16 +126,16 @@ describe "IATI basic form", :type => :feature do
 
       created_project = Project.all.first
       created_project.name.should               be == 'IATI test project'
-      created_project.id_in_organization.should be == '0123456789'
+      created_project.id_in_organization.should be == 123456789
       created_project.description.should        be == lorem_ipsum
-      created_project.organization_role.should  be == '1'
-      created_project.language.should           be == '1'
+      created_project.organization_role.should  be == 1
+      created_project.language.should           be == 1
       created_project.sector.should             be == ''
       created_project.subsector.should          be == ''
       created_project.start_date.should         be == ''
       created_project.end_date.should           be == ''
-      created_project.budget.should             be == '100000'
-      created_project.budget_currency.should    be == '1'
+      created_project.budget.should             be == 100000
+      created_project.budget_currency.should    be == 1
       created_project.contact_person.should     be == 'You'
       created_project.url.should                be == 'http://www.myiatiproject.org'
 
