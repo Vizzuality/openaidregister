42337883e7bc515b525804ab8bb58473af5837b3
Fernando Espinosa <fer@ferdev.com>
2013-02-07 17:00:07 +0100

    Adds edit project screen

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -15,12 +15,7 @@ class ProjectsController < ApplicationController
   end
 
   def new
-    #@project            = Project.new(:sectors    => '40,41,42',
-                                      #:subsectors => '199,203,206',
-                                      #:the_geom   => RGeo::GeoJSON.decode('{"type":"MultiPoint","coordinates":[[-3.00, 43.001], [43.00, 5.001], [3.00, 8.001]]}', :json_parser => :json))
-
-    @project            = Project.new
-    @url                = projects_path
+    @project            = Project.new(:user_id => current_user.id)
     @organization_roles = OpenAidRegister::ORGANIZATION_ROLES
     @languages          = OpenAidRegister::LANGUAGES
     @sectors            = OpenAidRegister::SECTORS
@@ -33,9 +28,7 @@ class ProjectsController < ApplicationController
 
     if @project.save
 
-      #flash[:notice] = 'Your project has been registered. Thanks!'
-
-      #Notifications.new_story(@project).deliver
+      flash[:info] = :new_project
 
       redirect_to edit_project_path(@project)
     else
@@ -44,6 +37,12 @@ class ProjectsController < ApplicationController
   end
 
   def edit
+    @project            = Project.find_by_id(params[:id])
+    @organization_roles = OpenAidRegister::ORGANIZATION_ROLES
+    @languages          = OpenAidRegister::LANGUAGES
+    @sectors            = OpenAidRegister::SECTORS
+    @subsectors         = OpenAidRegister::SUBSECTORS
+    @currencies         = OpenAidRegister::CURRENCIES
     @collaboration_types = OpenAidRegister::COLLABORATION_TYPES
     @aid_types           = OpenAidRegister::AID_TYPES
     @flow_types          = OpenAidRegister::FLOW_TYPES
